b26cb8d587ffc2a486e7dd858208378dafc5caf7
Everton <everton@versatecnologia.com.br>
2019-02-11 07:55:44 -0200

    Start a Project adding the Spring Boot

---
+++ src/main/java/br/com/notebookCoolerSmart/app/main/MainApplication.java
@@ -0,0 +1,14 @@
+package br.com.notebookCoolerSmart.app.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
+
